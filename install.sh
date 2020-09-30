e88af42639047296f3ae76cbc21fd95dc2f24e79
Arrogant Rabbit <chipmunk@arrogantrabbit.com>
2020-09-29 17:19:00 -0700

    Indentation in the here-documents

--- install.sh
+++ install.sh
@@ -15,11 +15,11 @@ readonly CPU_LIMIT_BATTERY=10
 readonly REQUESTED_CLI_VERSION="2.7.0"
 
 readonly LAUNCHD_BACKUP_SCHEDULE='
-    <key>StartCalendarInterval</key>
-    <dict>
-        <key>Minute</key>
-        <integer>0</integer>
-    </dict>
+	<key>StartCalendarInterval</key>
+	<dict>
+		<key>Minute</key>
+		<integer>0</integer>
+	</dict>
 '
 
 ## Should not need to modify anything below this line.
@@ -34,167 +34,164 @@ readonly LAUNCHD_BACKUP_NAME='com.duplicacy.backup'
 readonly DUPLICACY_CONFIG_DIR="${REPOSITORY_ROOT}/.duplicacy"
 readonly LAUNCHD_BACKUP_PLIST="/Library/LaunchDaemons/${LAUNCHD_BACKUP_NAME}.plist"
 
-
+## Helpers
+# Verify that utilities required are available
 function check_utilities_full()
 {
-    local error_code=0
-    for cmd in wget jq curl
-    do
-        if ! command -v $cmd > /dev/null 
-        then  
-            printf "Missing %s\n" "$cmd";
-            error_code=1;
-        fi
-    done 
-    return $error_code
+	local error_code=0
+	for cmd in wget jq curl
+	do
+		if ! command -v $cmd > /dev/null 
+		then  
+			printf "Missing %s\n" "$cmd";
+			error_code=1;
+		fi
+	done 
+	return $error_code
 }
 
 function check_utilities_lite()
 {
-    local error_code=0
-    for cmd in wget 
-    do
-        if ! command -v $cmd > /dev/null 
-        then  
-            printf "Missing %s\n" "$cmd";
-            error_code=1;
-        fi
-    done 
-    return $error_code
+	local error_code=0
+	for cmd in wget 
+	do
+		if ! command -v $cmd > /dev/null 
+		then  
+			printf "Missing %s\n" "$cmd";
+			error_code=1;
+		fi
+	done 
+	return $error_code
 }
 
+## Download duplicacy if needed
+#
 function update_duplicacy_binary()
 {
-    mkdir -p ${DUPLICACY_CONFIG_DIR}
-    
-    case "${REQUESTED_CLI_VERSION}" in 
-    Stable|stable) 
-        check_utilities_full || return $?
-        SELECTED_VERSION=$(curl -s 'https://duplicacy.com/latest_cli_version' |jq -r '.stable' 2>/dev/null) 
-        ;;
-    Latest|latest) 
-        check_utilities_full || return $?
-        SELECTED_VERSION=$(curl -s 'https://duplicacy.com/latest_cli_version' |jq -r '.latest' 2>/dev/null) 
-        ;;
-    *) 
-        check_utilities_lite || return $?
-        if [[ "${REQUESTED_CLI_VERSION}"  =~ ^[0-9.]+$ ]] ; then 
-            SELECTED_VERSION="${REQUESTED_CLI_VERSION}" 
-        else 
-            echo "Unrecognised update channel ${REQUESTED_CLI_VERSION}. Defaulting to Stable"; 
-            SELECTED_VERSION=$(curl -s 'https://duplicacy.com/latest_cli_version' |jq -r '.stable' 2>/dev/null) 
-        fi 
-        ;;
-    esac
-    
-    LOCAL_EXECUTABLE_NAME="${DUPLICACY_CONFIG_DIR}/duplicacy_osx_x64_${SELECTED_VERSION}"
+	mkdir -p "${DUPLICACY_CONFIG_DIR}"
+	
+	case "${REQUESTED_CLI_VERSION}" in 
+	Stable|stable) 
+		check_utilities_full || return $?
+		SELECTED_VERSION=$(curl -s 'https://duplicacy.com/latest_cli_version' |jq -r '.stable' 2>/dev/null) 
+		;;
+	Latest|latest) 
+		check_utilities_full || return $?
+		SELECTED_VERSION=$(curl -s 'https://duplicacy.com/latest_cli_version' |jq -r '.latest' 2>/dev/null) 
+		;;
+	*) 
+		check_utilities_lite || return $?
+		if [[ "${REQUESTED_CLI_VERSION}"  =~ ^[0-9.]+$ ]] ; then 
+			SELECTED_VERSION="${REQUESTED_CLI_VERSION}" 
+		else 
+			echo "Unrecognised update channel ${REQUESTED_CLI_VERSION}. Defaulting to Stable"; 
+			SELECTED_VERSION=$(curl -s 'https://duplicacy.com/latest_cli_version' |jq -r '.stable' 2>/dev/null) 
+		fi 
+		;;
+	esac
+	
+	LOCAL_EXECUTABLE_NAME="${DUPLICACY_CONFIG_DIR}/duplicacy_osx_x64_${SELECTED_VERSION}"
 
-    if [ -f "${LOCAL_EXECUTABLE_NAME}" ] 
-    then
-        echo "Version ${SELECTED_VERSION} is up to date"
-    else
-        DOWNLOAD_URL="${DOWNLOAD_ROOT}/v${SELECTED_VERSION}/duplicacy_osx_x64_${SELECTED_VERSION}"
-        if wget -O "${LOCAL_EXECUTABLE_NAME}" "${DOWNLOAD_URL}" ; then 
-            chmod u=rwx,g=rx,o=rx "${LOCAL_EXECUTABLE_NAME}"
-            echo "Updated to ${SELECTED_VERSION}"
-        else
-            echo "Could not download ${DOWNLOAD_URL}"
-            rm -f "${LOCAL_EXECUTABLE_NAME}"
-        return 1
-        fi
-    fi 
-    return 0
+	if [ -f "${LOCAL_EXECUTABLE_NAME}" ] 
+	then
+		echo "Version ${SELECTED_VERSION} is up to date"
+	else
+		DOWNLOAD_URL="${DOWNLOAD_ROOT}/v${SELECTED_VERSION}/duplicacy_osx_x64_${SELECTED_VERSION}"
+		if wget -O "${LOCAL_EXECUTABLE_NAME}" "${DOWNLOAD_URL}" ; then 
+			chmod u=rwx,g=rx,o=rx "${LOCAL_EXECUTABLE_NAME}"
+			echo "Updated to ${SELECTED_VERSION}"
+		else
+			echo "Could not download ${DOWNLOAD_URL}"
+			rm -f "${LOCAL_EXECUTABLE_NAME}"
+		return 1
+		fi
+	fi 
+	return 0
 }
 
+## Write out the launch plists
 function prepare_launchd_backup_plist()
 {
+	echo "Writing out ${LAUNCHD_BACKUP_PLIST}"
+	mkdir -p ${LOGS_PATH}
 
-echo "Writing out ${LAUNCHD_BACKUP_PLIST}"
+	cat > "${LAUNCHD_BACKUP_PLIST}" <<- EOF
+	<?xml version="1.0" encoding="UTF-8"?>
+	<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+	<plist version="1.0">
+	<dict>
+	    <key>StandardOutPath</key>
+	    <string>${LOGS_PATH}/${LAUNCHD_BACKUP_NAME}.out.log</string>
 
-mkdir -p ${LOGS_PATH}
+	    <key>StandardErrorPath</key>
+	    <string>${LOGS_PATH}/${LAUNCHD_BACKUP_NAME}.err.log</string>
 
-cat > "${LAUNCHD_BACKUP_PLIST}" << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>StandardOutPath</key>
-    <string>${LOGS_PATH}/${LAUNCHD_BACKUP_NAME}.out.log</string>
+	    <key>Label</key>
+	    <string>${LAUNCHD_BACKUP_NAME}</string>
 
-    <key>StandardErrorPath</key>
-    <string>${LOGS_PATH}/${LAUNCHD_BACKUP_NAME}.err.log</string>
+	    <key>WorkingDirectory</key>
+	    <string>${REPOSITORY_ROOT}</string>
 
-    <key>Label</key>
-    <string>${LAUNCHD_BACKUP_NAME}</string>
+	    <key>Program</key>
+	    <string>${DUPLICACY_CONFIG_DIR}/backup.sh</string>
 
-    <key>WorkingDirectory</key>
-    <string>${REPOSITORY_ROOT}</string>
-
-    <key>Program</key>
-    <string>${DUPLICACY_CONFIG_DIR}/backup.sh</string>
-
-    ${LAUNCHD_BACKUP_SCHEDULE}
-    
-</dict>
-</plist>
-EOF
-
-return 0
+	    ${LAUNCHD_BACKUP_SCHEDULE}
+	
+	</dict>
+	</plist>
+	EOF
+	return 0
 }
-
+ 
+## Write out throttler scripts
+#
 function prepare_duplicacy_scripting()
 {
+	BACKUP="${DUPLICACY_CONFIG_DIR}/backup.sh"
+	echo "Writing out ${BACKUP}"
 
-BACKUP="${DUPLICACY_CONFIG_DIR}/backup.sh"
 
-echo "Writing out ${BACKUP}"
+	cat >> "${BACKUP}" <<- EOF
+	#!/bin/bash
+	CPU_LIMIT_CORE_AC=${CPU_LIMIT_AC}
+	CPU_LIMIT_CORE_BATTERY=${CPU_LIMIT_BATTERY}
+	DUPLICASY_CLI_PATH="${LOCAL_EXECUTABLE_NAME}"
+	EOF
 
-cat > "${BACKUP}" << 'EOF'
-#!/bin/bash
-EOF
+	cat >> "${BACKUP}" <<- 'EOF'
+	case "$(pmset -g batt | grep 'Now drawing from')" in
+	*Battery*) CPU_LIMIT_CORE=${CPU_LIMIT_CORE_BATTERY} ;;
+	*)		   CPU_LIMIT_CORE=${CPU_LIMIT_CORE_AC} ;;
+	esac
 
-cat >> "${BACKUP}" << EOF
-CPU_LIMIT_CORE_AC=${CPU_LIMIT_AC}
-CPU_LIMIT_CORE_BATTERY=${CPU_LIMIT_BATTERY}
-EOF
+	function terminator() {
+	    kill -TERM "${duplicacy}" 2>/dev/null
+	    kill -TERM "${throttler}" 2>/dev/null
+	}
 
-cat >> "${BACKUP}" << 'EOF'
-case "$(pmset -g batt | grep 'Now drawing from')" in
-*Battery*) CPU_LIMIT_CORE=${CPU_LIMIT_CORE_BATTERY} ;;
-*)         CPU_LIMIT_CORE=${CPU_LIMIT_CORE_AC} ;;
-esac
+	trap terminator SIGHUP SIGINT SIGQUIT SIGTERM EXIT
 
-function terminator() {
-    kill -TERM "${duplicacy}" 2>/dev/null
-    kill -TERM "${throttler}" 2>/dev/null
-}
+	"${DUPLICASY_CLI_PATH}" backup &
+	duplicacy=$!
+	/usr/local/bin/cpulimit --limit=${CPU_LIMIT_CORE} --include-children --pid=${duplicacy} &
+	throttler=$!
 
-trap terminator SIGHUP SIGINT SIGQUIT SIGTERM EXIT
-EOF
+	wait ${throttler}
 
-cat >> "${BACKUP}" << EOF
-"${LOCAL_EXECUTABLE_NAME}" backup &
-EOF
+	EOF
 
-cat >> "${BACKUP}" << 'EOF'
-duplicacy=$!
-/usr/local/bin/cpulimit --limit=${CPU_LIMIT_CORE} --include-children --pid=${duplicacy} &
-throttler=$!
-wait ${throttler}
-EOF
-
-chmod +x "${BACKUP}"|| exit $?
-return 0;
+    chmod +x "${BACKUP}"|| exit $?
+    return 0;
 }
 
 if [[ $(id -u) != 0 ]]; then
-    sudo -p 'Restarting as root, password: ' bash $0 "$@"
-    exit $?
+	sudo -p 'Restarting as root, password: ' bash $0 "$@"
+	exit $?
 fi
 
 if [ ! -f "${DUPLICACY_CONFIG_DIR}/preferences" ] ; then 
-    echo "Please initialize duplicacy repository at ${REPOSITORY_ROOT} first."
-    exit 2; 
+	echo "Please initialize duplicacy repository at ${REPOSITORY_ROOT} first."
+	exit 2; 
 fi 
 
 echo "Stopping and unloading existing daemon"
